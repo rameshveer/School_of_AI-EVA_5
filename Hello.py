db9cd1fdd604c1851cd0cef8115fb97b33ef8bc0
Ramesh Veerusikku <rameshveerfeb27@gmail.com>
2020-07-16 11:54:37 +0530

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 28 23:04:17 2017
+
+@author: DINESH
+"""
+
+name = "Hello World"
+print(name)
+name = name.replace('Hello', "Hi")
+print(name)
+
+array = [1,2,3]
+type(array)
+
